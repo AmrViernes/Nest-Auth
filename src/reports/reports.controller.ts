8810d2e43c4109cf05dc357a182f0d372e31d7da
AmrViernes <amraromoro@hotmail.com>
2024-05-01 10:13:57 +0300

    feat: added reports controller

--- src/reports/reports.controller.ts
+++ src/reports/reports.controller.ts
@@ -1,20 +1,36 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards, Put, Query } from '@nestjs/common';
 import { ReportsService } from './reports.service';
 import { CreateReportDto } from './dto/create-report.dto';
 import { UpdateReportDto } from './dto/update-report.dto';
+import { AuthGuard } from 'src/auth/auth.guard';
+import { CurrentUser } from 'src/decorators/current-user/current-user.decorator';
+import { UserEntity } from 'src/users/entities/user.entity';
+import { Serialize } from 'src/interceptors/serialize/serialize.interceptor';
+import { ReportDto } from './dto/report.dto';
+import { ApproveReportDto } from './dto/approve-report.dto';
+import { AdminGuard } from 'src/guards/admin/admin.guard';
+import { GetEstimateDto } from './dto/get-estimate.dto';
 
 @Controller('reports')
 export class ReportsController {
   constructor(private readonly reportsService: ReportsService) {}
 
   @Post()
-  create(@Body() createReportDto: CreateReportDto) {
-    return this.reportsService.create(createReportDto);
+  //@UseGuards(AuthGuard)
+  @Serialize(ReportDto)
+  create(@Body() createReportDto: CreateReportDto, @CurrentUser() user: UserEntity) {
+    return this.reportsService.create(createReportDto, user);
   }
 
   @Get()
   findAll() {
     return this.reportsService.findAll();
+  }
+
+  @Get('/estimate')
+  getEstimate(@Query() query: GetEstimateDto){
+    return this.reportsService.createEstimate(query)
+    
   }
 
   @Get(':id')
@@ -24,7 +40,13 @@ export class ReportsController {
 
   @Patch(':id')
   update(@Param('id') id: string, @Body() updateReportDto: UpdateReportDto) {
-    return this.reportsService.update(+id, updateReportDto);
+    return this.reportsService.update(id, updateReportDto);
+  }
+
+  @Put(':id')
+  @UseGuards(AdminGuard)
+  approveReport(@Param('id') id: string, @Body() approveReportDto: ApproveReportDto) {
+    return this.reportsService.approveReport(id, approveReportDto)
   }
 
   @Delete(':id')
