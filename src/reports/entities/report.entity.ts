7bb5fa7d1712d1331f7e9a079caf09e5552c6861
AmrViernes <amraromoro@hotmail.com>
2024-05-01 10:17:54 +0300

    feat: added report entity

--- src/reports/entities/report.entity.ts
+++ src/reports/entities/report.entity.ts
@@ -1,11 +1,37 @@
-import { Entity, Column, PrimaryGeneratedColumn,CreateDateColumn, UpdateDateColumn, DeleteDateColumn } from "typeorm";
+import { UserEntity } from "src/users/entities/user.entity";
+import { Entity, Column, PrimaryGeneratedColumn,CreateDateColumn, UpdateDateColumn, DeleteDateColumn, ManyToOne } from "typeorm";
 
 @Entity()
 export class ReportEntity {
     @PrimaryGeneratedColumn("uuid")
     id: string;
+
+    @Column({default: false})
+    approved: boolean
+
+    @Column()
+    make: string
+
+    @Column()
+    model: string
+
+    @Column()
+    year: number
+
+    @Column()
+    kilometers: number
+
+    @Column()
+    lat: number
+
+    @Column()
+    lang: number 
+    
     @Column()
     price: number;
+
+    @ManyToOne(() => UserEntity, user => user.reports)
+    user: UserEntity
 
     @CreateDateColumn()
     createdAt: Date;
