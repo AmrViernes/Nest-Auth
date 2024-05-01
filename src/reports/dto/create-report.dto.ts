1f965ff532626562054696d0894ea512382022ca
AmrViernes <amraromoro@hotmail.com>
2024-05-01 10:14:42 +0300

    feat: added create report dto

--- src/reports/dto/create-report.dto.ts
+++ src/reports/dto/create-report.dto.ts
@@ -1,1 +1,43 @@
-export class CreateReportDto {}
+import {
+  IsLatitude,
+  IsLongitude,
+  IsNotEmpty,
+  IsNumber,
+  IsString,
+  Max,
+  Min,
+} from 'class-validator';
+
+export class CreateReportDto {
+  @IsString()
+  @IsNotEmpty()
+  make: string;
+
+  @IsString()
+  @IsNotEmpty()
+  model: string;
+
+  @IsNumber()
+  @Min(1930)
+  @Max(new Date().getFullYear())
+  year: number;
+
+  @IsNumber()
+  @Min(0)
+  @Max(1000000)
+  kilometers: number;
+
+  @IsNumber()
+  @IsLatitude()
+  lat: number;
+
+  @IsNumber()
+  @IsLongitude()
+  lang: number;
+
+  @IsNumber()
+  @IsNotEmpty()
+  @Min(0)
+  @Max(10000000)
+  price: number;
+}
