7a1cc0bf2d52e5df617331f5de2aaa081b8c3e83
AmrViernes <amraromoro@hotmail.com>
2024-05-01 10:12:52 +0300

    feat: added admin guard

---
+++ src/guards/admin/admin.guard.ts
@@ -0,0 +1,15 @@
+import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+
+@Injectable()
+export class AdminGuard implements CanActivate {
+  canActivate(context: ExecutionContext) {
+    const request = context.switchToHttp().getRequest();
+
+    if (!request.currentUser) return false;
+    if (request.currentUser.role === 2) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+}
