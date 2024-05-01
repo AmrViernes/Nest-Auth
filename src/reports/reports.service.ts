40a7d3912bda30ded2429646b72154da07e5db92
AmrViernes <amraromoro@hotmail.com>
2024-05-01 10:14:10 +0300

    feat: added reports service

--- src/reports/reports.service.ts
+++ src/reports/reports.service.ts
@@ -1,23 +1,49 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateReportDto } from './dto/create-report.dto';
 import { UpdateReportDto } from './dto/update-report.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { ReportEntity } from './entities/report.entity';
+import { Repository } from 'typeorm';
+import { UserEntity } from 'src/users/entities/user.entity';
+import { ApproveReportDto } from './dto/approve-report.dto';
+import { GetEstimateDto } from './dto/get-estimate.dto';
 
 @Injectable()
 export class ReportsService {
-  create(createReportDto: CreateReportDto) {
-    return 'This action adds a new report';
+  constructor(
+    @InjectRepository(ReportEntity) private repo: Repository<ReportEntity>,
+  ) {}
+
+  create(createReportDto: CreateReportDto, user: UserEntity) {
+    const report = this.repo.create(createReportDto);
+    report.user = user;
+    return this.repo.save(report);
+  }
+
+  createEstimate(estimateDTO: GetEstimateDto) {
+    return this.repo.createQueryBuilder()
+    .select('*')
+    .where('make = :make', {make: estimateDTO.make})
+    .getRawMany()
   }
 
   findAll() {
-    return `This action returns all reports`;
+    return this.repo.find();
   }
 
   findOne(id: number) {
     return `This action returns a #${id} report`;
   }
 
-  update(id: number, updateReportDto: UpdateReportDto) {
+  update(id: string, updateReportDto: UpdateReportDto) {
     return `This action updates a #${id} report`;
+  }
+
+  async approveReport(id: string, approveReportDto: ApproveReportDto) {
+    const report = await this.repo.findOne({ where: { id } });
+    if (!report) throw new NotFoundException('Report not found');
+    report.approved = approveReportDto.approved;
+    return this.repo.save(report);
   }
 
   remove(id: number) {
