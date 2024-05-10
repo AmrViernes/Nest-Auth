0c60621efe4926aef142c06a8f363773d7732b01
Amr El-Desoky <37755092+AmrViernes@users.noreply.github.com>
2024-05-10 22:40:44 +0300

    Update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,7 +6,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { ReportsModule } from './reports/reports.module';
 import { UsersModule } from './users/users.module';
 import { UserEntity } from './users/entities/user.entity';
-import { ReportEntity } from './reports/entities/report.entity';
+import { ReportEntity } from './reports/entities/report.entity'
 
 @Module({
   imports: [
