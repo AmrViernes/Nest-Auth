ec83be22d389a11e7a2abd57ae86776add8c55c1
Amr El-Desoky <37755092+AmrViernes@users.noreply.github.com>
2024-05-11 20:20:59 +0300

    Update users.service.spec.ts

--- src/users/users.service.spec.ts
+++ src/users/users.service.spec.ts
@@ -10,7 +10,7 @@ describe('UsersService', () => {
     }).compile();
 
     service = module.get<UsersService>(UsersService);
-  });
+  })
 
   it('should be defined', () => {
     expect(service).toBeDefined();
