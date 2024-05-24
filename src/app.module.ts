a7d636efe25c6676fe694f1e8a7d2daf86518f7a
Amr El-Desoky <37755092+AmrViernes@users.noreply.github.com>
2024-05-24 23:20:13 +0300

    Update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
 import {ConfigModule, ConfigService} from '@nestjs/config'
-import { AppController } from './app.controller';
+import { AppController } from './app.controller'
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ReportsModule } from './reports/reports.module';
